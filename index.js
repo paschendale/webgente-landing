dc7138e112c96ebb314ec23baaf4ec4fb6bd90f6
guirosemberg <68906555+guirosemberg@users.noreply.github.com>
2021-05-10 22:01:38 -0300

    Commit inicial
    Estrutura do site

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express"); //importando o express
+const app = express(); //iniciando o express
+
+//Dizendo para o Express utilizar o EJS como view engine
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+
+
+
+app.listen(3000,() => {console.log("Landing page do WebGENTE rodando na porta 3000");});
+
+
+
+
